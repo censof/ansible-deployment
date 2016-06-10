1dc5fe9ec59cda14524e4c2ec78d9aa96bcbcdad
Low Kian Seong <lukman.low@censof.com>
2016-06-10 11:27:51 +0800

    re #19 Correcting wrong path for nginx.conf

--- tests/spec/test_ansible_spec.rb
+++ tests/spec/test_ansible_spec.rb
@@ -5,13 +5,11 @@ set :backend, :exec
 packages = ["git", "unzip", "epel-release", "nginx"]
 
 describe "Make sure base packages installed" do
-
 	packages.each do |base|
 	   describe package(base) do
 	      it { should be_installed }
 	   end
 	end
-
 end
 
 describe "path should exist" do
@@ -20,18 +18,19 @@ describe "path should exist" do
 	    it { should be_executable }
 	end
 
-	describe file("/etc/nginx/sites-enabled") do
-	    it { should exist }
-	end
+	# describe file("/etc/nginx/sites-enabled") do
+	#     it { should exist }
+	# end
 
-	describe file("/etc/nginx/sites-available") do
-	    it { should exist }
-	end
+	# describe file("/etc/nginx/sites-available") do
+	#     it { should exist }
+	# end
 
-	describe file("/etc/nginx/sites-enabled/eclaim_nginx.conf") do
-	    it { should exist }
-            it { should be_symlink }
-	end
+	# describe file("/etc/nginx/
+	# sites-enabled/eclaim_nginx.conf") do
+	#     it { should exist }
+  #           it { should be_symlink }
+	# end
 
 	describe file("/etc/nginx/conf.d/eclaim.conf") do
 			it { should exist }
@@ -43,6 +42,11 @@ describe "path should exist" do
 
 	describe file("/etc/environment") do
 	    it { should exist }
+	end
+
+	describe file("/etc/rc.d/init.d/uwsgi") do
+	    it { should exist }
+	    it { should be_executable }
 	end
 
 	describe file("/opt/miniconda2/bin/uwsgi") do
