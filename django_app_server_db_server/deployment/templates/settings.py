a2ecbfedc2355bdc42304a636c0b59afdd0098c0
Low Kian Seong <lukman.low@censof.com>
2016-09-14 21:53:01 +0800

    re #92 fix problem with multi host on redhat

---
+++ django_app_server_db_server/deployment/templates/settings.py
@@ -0,0 +1,21 @@
+"""Production settings and globals.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.8/ref/settings/
+"""
+
+from .modules import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'eclaim',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '{{ db_server_ip }}',
+        'PORT': '',
+    }
+}
