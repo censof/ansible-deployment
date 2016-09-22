87fde2c7bdb717742efb78a414986fdf5f527c5c
NorJuliana <norjuliana@censof.com>
2016-09-22 08:54:39 +0800

    Add option in apt-get not to prompt replacement config

--- installubuntu.sh
+++ installubuntu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #install sudo sshpass and curl
- apt-get install -y sudo sshpass curl
+# apt-get install -y sudo sshpass curl
+apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y sudo sshpass curl
 
 #ssh-rsa
  echo -e 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDF1XPR2tXoWMpywpenL6EyOn9tpGveZegVXZS7QUfUwsYowc+JdHDJv/k4sjaHzPNpFEm9SHI3nPLUcQ5cs+/uEh75AqMocqi/ov87o8+vDhLEcpmoWfA+W4HZ6KS1vzjxGw9XIw7ZvwCuPIXHorJZZCsFzanzl71Z+OHrvvU1fxVlp0bwEV42gAn5CtM7S33ZCuk2YuG5wQwrjcsdwE6a0ATgG84buXL11Vj1XRrZ+ybj679GOd/uhUJImrXFapHeob/arsCiQnPGCwCvVohd2anm1DdAm/SPQjUJFt81VAOKG2kBw8hfJgg9p1l3ZvFS0EP6v8wyAuU9Tje6xRrD lowks@lowkster-laptop' >> ~/.ssh/id_rsa.pub
