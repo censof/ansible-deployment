92da0636cba04f25c4127eb672246ab2d8caf1a3
Low Kian Seong <lowks@users.noreply.github.com>
2016-09-27 10:06:28 +0800

    Update installubuntu.sh
    debug

--- installubuntu.sh
+++ installubuntu.sh
@@ -5,13 +5,13 @@
 apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install -y sudo sshpass curl
 
 #ssh-rsa
- echo -e 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXN7FamXjkhM5BUDhkJ6pyxetos1b/MJ7J6KwI49Reh72LNfiXcW/SU2bF6zTAorDOIiqbW+dsI7PNtc5MxNCCvDoarCcn15p6KFWYSvC6r3krNKdqSKcRvQXKXvWRWWkYzCZVFqHoTDJTGThFzfFUBNEpBU84sMovuU5HVAmbrED3d/nfXumc8c370ubgzDGZkWPNCe+W5RQua6p50GQ2fvCMlEexc/hzGVyP3CjY8ADQQzy+K5BrxYUyINV8vbFDo0dW+ThIbzDQEvRqdo5nge74nKY5WJDhXu3pZDFXS3dnd4B0Heiz2O1j582Z55TjS3/njhwGvcFlvIABfloJ juliana@juliana-Latitude-E5510' >> ~/.ssh/id_rsa.pub
+# echo -e 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXN7FamXjkhM5BUDhkJ6pyxetos1b/MJ7J6KwI49Reh72LNfiXcW/SU2bF6zTAorDOIiqbW+dsI7PNtc5MxNCCvDoarCcn15p6KFWYSvC6r3krNKdqSKcRvQXKXvWRWWkYzCZVFqHoTDJTGThFzfFUBNEpBU84sMovuU5HVAmbrED3d/nfXumc8c370ubgzDGZkWPNCe+W5RQua6p50GQ2fvCMlEexc/hzGVyP3CjY8ADQQzy+K5BrxYUyINV8vbFDo0dW+ThIbzDQEvRqdo5nge74nKY5WJDhXu3pZDFXS3dnd4B0Heiz2O1j582Z55TjS3/njhwGvcFlvIABfloJ juliana@juliana-Latitude-E5510' >> ~/.ssh/id_rsa.pub
 
 #copy something
-cp /root/django_deployment/id_travis ~/.ssh/id_rsa
+# cp /root/django_deployment/id_travis ~/.ssh/id_rsa
 
 #whatever
-chmod 400 ~/.ssh/id_rsa
+# chmod 400 ~/.ssh/id_rsa
 /opt/miniconda2/bin/conda install --yes -c kbroughton ansible
 
 #installl freetype fontconfig
