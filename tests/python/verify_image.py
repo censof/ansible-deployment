9bf43b1a708dd1880cdae3d02135cf4679cfc7ac
Low Kian Seong <lukman.low@censof.com>
2016-11-14 15:01:31 +0800

    Changes because BM now default language

--- tests/python/verify_image.py
+++ tests/python/verify_image.py
@@ -24,7 +24,7 @@ class TestClaimsLogin(unittest.TestCase):
         self.driver.execute_script("set_language('ms')")
         sleep(5)
         self.assertEqual(self.driver.find_element_by_id("logo").text,
-                         u'Staff Claims')
+                         u'Tuntutan Staf')
 
     def tearDown(self):
         self.driver.get('http://%s/eclaim/logout' % self.ip)
